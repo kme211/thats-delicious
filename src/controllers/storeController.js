04926a4cbf3806285e9bd607562099e61d95e66f
Keari Eggers <kungfu.keari@gmail.com>
2017-06-01 13:40:27 -0500

    complete vid 17

--- src/controllers/storeController.js
+++ src/controllers/storeController.js
@@ -26,6 +26,7 @@ exports.editStore = async (req, res) => {
 }
 
 exports.updateStore = async (req, res) => {
+  req.body.location.type = 'Point';
   const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
     new: true, // returns new store instead of old one
     runValidators: true // force model to run required validators
