285f3b6b8b3d6703fc5b107ec12191c090dd81ca
Keari Eggers <kungfu.keari@gmail.com>
2017-06-05 15:31:28 -0500

    complete vid 27
    Password reset flow.
    
    I also moved the check reset token functionality into its own middleware in authController.

--- src/routes/index.js
+++ src/routes/index.js
@@ -43,4 +43,16 @@ router.get('/logout', authController.logout);
 router.get('/account', authController.isLoggedIn, userController.account);
 router.post('/account', catchErrors(userController.updateAccount));
 
+router.post('/account/forgot', catchErrors(authController.forgot));
+
+router.get('/account/reset/:token', 
+  catchErrors(authController.checkResetToken),
+  catchErrors(authController.reset)
+);
+router.post('/account/reset/:token', 
+  authController.confirmPasswords,
+  catchErrors(authController.checkResetToken),
+  catchErrors(authController.update)
+);
+
 module.exports = router;
